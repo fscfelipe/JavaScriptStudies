f32d334ac328648ffbf512d68e9670b30087adce
fscfelipe <felipesobreira.c@hotmail.com>
2017-04-27 16:07:39 -0300

    JavaScript object example

---
+++ JavaScriptObjects/script.js
@@ -0,0 +1,43 @@
+//VETOR DE OBJETOS
+//Para acessar = students.[0].nome
+
+var students = [
+	{
+		name: 'Felipe Sobreira',
+		track: 'Student',
+		achievements: 120,
+		points: 543
+	},
+	{
+		name: 'Jonas Harrison',
+		track: 'Developer',
+		achievements: 133,
+		points: 456
+
+	},
+	{
+		name: 'Marina Pereira',
+		track: 'Manager',
+		achievements: 179,
+		points: 325
+	},
+	{
+		name: 'Ching ling',
+		track: 'Do everything',
+		achievements: 346,
+		points: 782
+	},
+	{
+		name: 'Little lalita',
+		track: 'Secretary',
+		achievements: 50,
+		points: 169
+	},
+	{
+		name: 'Sujiro kifuja',
+		track: 'Ninja',
+		achievements: 420,
+		points: 420
+	}
+
+];
