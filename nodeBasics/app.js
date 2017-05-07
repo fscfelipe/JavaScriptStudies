dd990758e45637ff0b07ec8749e56d10f0e81501
fscfelipe <felipesobreira.c@hotmail.com>
2017-05-07 01:08:40 -0300

    tratado exceções

--- nodeBasics/app.js
+++ nodeBasics/app.js
@@ -4,29 +4,47 @@
 // Require https module
 const https = require('https');
 
+// Require http module for status code
+const http = require('http');
+
 function printMessage(username, badgeCount, point) {
     const message = username + " has " + badgeCount + " total badge(s) and " + point + " points in JavaScript";
     console.log(message);
 }
 
 function getProfile(username) {
-    // Connect to the API URL (https://teamtreehouse.com/username.json)
-    const request = https.get("https://teamtreehouse.com/" + username + ".json", response => {
-        let body = "";
-        // Read the data
-        response.on('data', data => {
-            body += data.toString();
-        })
+    try {
+        // Connect to the API URL (https://teamtreehouse.com/username.json)
+        const request = https.get("https://teamtreehouse.com/" + username + ".json", response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                // Read the data
+                response.on('data', data => {
+                    body += data.toString();
+                })
 
-        response.on('end', () => {
-            // Parse the data
-            const profile = JSON.parse(body);
-            // Print the data
-            printMessage(username, profile.badges.length, profile.points.JavaScript);
-        })
+                response.on('end', () => {
+                    try {
+                        // Parse the data
+                        const profile = JSON.parse(body);
+                        // Print the data
+                        printMessage(username, profile.badges.length, profile.points.JavaScript);
+                    } catch (error) {
+                        console.error(error.message);
+                    }
+                });
+            } else {
+                const message = "There was an error getting the profile for " + username + ` (${http.STATUS_CODES[response.statusCode]})`;
+                const statusCodeError = new Error(message);
+                console.error(statusCodeError.message);
+            }
+        });
 
-
-    });
+        request.on('error', error => console.error("Problem with request: " + error.message));
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
+// change the profile name to do differente tests
 getProfile("chalkers");
