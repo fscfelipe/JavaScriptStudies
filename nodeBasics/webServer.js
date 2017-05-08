439f4a9a2278470c3684126ce2ec78b8dc901351
fscfelipe <felipesobreira.c@hotmail.com>
2017-05-07 21:31:09 -0300

    brincando com rotas

---
+++ nodeBasics/webServer.js
@@ -0,0 +1,17 @@
+const router = require("./router.js");
+
+// Create a web server
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    router.home(request, response);
+    router.user(request, response);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
